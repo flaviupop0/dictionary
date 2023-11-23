378aec3631bf8115eaca352ef661ffdf4fbddad1
flaviupop0 <flaviupop2001@gmail.com>
2023-11-23 13:14:06 +0200

    Added delete functionality

--- script.js
+++ script.js
@@ -8,38 +8,56 @@ function createWordContainer() {
 }
 
 function toggleErrorModal(errorType) {
-    let errorModal = new bootstrap.Modal(document.getElementById(errorType));
+    const errorModal = new bootstrap.Modal(document.getElementById(errorType));
     errorModal.toggle();
 }
 
 function addWord() {
-    let wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
-    let definitionInput = document.querySelector(".cutom-input:nth-of-type(2)");
+    const wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
+    const definitionInput = document.querySelector(".cutom-input:nth-of-type(2)");
     if (wordInput.value.trim() === "") {
         toggleErrorModal("errorModal");
         return;
     }
     createWordContainer();
-    let existingWords = document.querySelectorAll(".word");
+    const existingWords = document.querySelectorAll(".word");
     for (let i = 0; i < existingWords.length; ++i) {
         if (existingWords[i].textContent.toLowerCase() === wordInput.value.trim().toLowerCase()) {
             toggleErrorModal("duplicateWordModal");
             return;
         }
     }
-    let newWord = document.createElement("p");
+    const newWordContainer = document.createElement("div");
+    const newWord = document.createElement("p");
+    const deleteButton = document.createElement("button");
+    newWordContainer.classList.add("word-container");
     newWord.innerHTML = '<span class="word">' + wordInput.value + '</span><span class="separator"> | </span><span class="definition">' + definitionInput.value + "</span>";
-    document.getElementById("wordContainer").appendChild(newWord);
+    deleteButton.classList.add("btn", "btn-outline-danger", "delete-button");
+    deleteButton.innerHTML = "🗑️ Delete";
+    deleteButton.addEventListener("click", function () {
+        newWordContainer.remove();
+        destroyWordContainer();
+    });
+    newWordContainer.appendChild(newWord);
+    newWordContainer.appendChild(deleteButton);
+    document.getElementById("wordContainer").appendChild(newWordContainer);
     wordInput.value = "";
     definitionInput.value = "";
 }
 
+function destroyWordContainer() {
+    const wordContainer = document.getElementById("wordContainer");
+    if (wordContainer && wordContainer.children.length === 0) {
+        wordContainer.remove();
+    }
+}
+
 function searchWord() {
-    let wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
-    let searchValue = wordInput.value.trim().toLowerCase();
-    let existingWords = document.querySelectorAll(".word");
+    const wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
+    const searchValue = wordInput.value.trim().toLowerCase();
+    const existingWords = document.querySelectorAll(".word");
     for (let i = 0; i < existingWords.length; ++i) {
-        let word = existingWords[i].textContent.toLowerCase();
+        const word = existingWords[i].textContent.toLowerCase();
         if (word === searchValue) {
             existingWords[i].scrollIntoView({ behavior: "smooth", block: "center" });
             existingWords[i].classList.add("highlighted");
