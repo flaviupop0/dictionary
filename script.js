eeb4cc1cd42f6472b0393bb0459fa718cf5cc89f
flaviupop0 <flaviupop2001@gmail.com>
2023-11-23 13:48:50 +0200

    Added array for a better functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let words = [];
+
 function createWordContainer() {
     let wordContainer = document.getElementById("wordContainer");
     if (!wordContainer) {
@@ -15,26 +17,28 @@ function toggleErrorModal(errorType) {
 function addWord() {
     const wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
     const definitionInput = document.querySelector(".cutom-input:nth-of-type(2)");
+    const wordValue = wordInput.value.toLowerCase;
     if (wordInput.value.trim() === "") {
         toggleErrorModal("errorModal");
         return;
     }
-    createWordContainer();
-    const existingWords = document.querySelectorAll(".word");
-    for (let i = 0; i < existingWords.length; ++i) {
-        if (existingWords[i].textContent.toLowerCase() === wordInput.value.trim().toLowerCase()) {
-            toggleErrorModal("duplicateWordModal");
-            return;
-        }
+    if (words.includes(wordValue)) {
+        toggleErrorModal("duplicateWordModal");
+        return;
     }
+    createWordContainer();
     const newWordContainer = document.createElement("div");
     const newWord = document.createElement("p");
     const deleteButton = document.createElement("button");
+    words.push(wordValue);
     newWordContainer.classList.add("word-container");
     newWord.innerHTML = '<span class="word">' + wordInput.value + '</span><span class="separator"> | </span><span class="definition">' + definitionInput.value + "</span>";
     deleteButton.classList.add("btn", "btn-outline-danger", "delete-button");
     deleteButton.innerHTML = "🗑️ Delete";
     deleteButton.addEventListener("click", function () {
+        if (index !== -1) {
+            words.splice(index, 1);
+        }
         newWordContainer.remove();
         destroyWordContainer();
     });
@@ -56,16 +60,17 @@ function searchWord() {
     const wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
     const searchValue = wordInput.value.trim().toLowerCase();
     const existingWords = document.querySelectorAll(".word");
-    for (let i = 0; i < existingWords.length; ++i) {
-        const word = existingWords[i].textContent.toLowerCase();
-        if (word === searchValue) {
-            existingWords[i].scrollIntoView({ behavior: "smooth", block: "center" });
-            existingWords[i].classList.add("highlighted");
-            setTimeout(() => {
-                existingWords[i].classList.remove("highlighted");
-            }, 2000);
-            return;
-        }
+    const foundWord = Array.from(existingWords).find((wordElement) => {
+        const word = wordElement.textContent.toLowerCase();
+        return word === searchValue;
+    });
+    if (foundWord) {
+        foundWord.scrollIntoView({ behavior: "smooth", block: "center" });
+        foundWord.classList.add("highlighted");
+        setTimeout(() => {
+            foundWord.classList.remove("highlighted");
+        }, 2000);
+    } else {
+        toggleErrorModal("notExist");
     }
-    toggleErrorModal("notExist");
 }
